aa80017db7d43360eb6ccae1f0b19856ca5fd199
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-21 21:33:14 +0900

    088. FutureBuilder로 사진 표시하기

--- lib/my_gallery_app.dart
+++ lib/my_gallery_app.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -9,29 +10,44 @@ class MyGalleryApp extends StatefulWidget {
 }
 
 class _MyGalleryAppState extends State<MyGalleryApp> {
-  final ImagePicker _picker =ImagePicker();
+  final ImagePicker _picker = ImagePicker();
   List<XFile>? images;
 
   @override
   void initState() {
-
     super.initState();
     loadImages();
   }
-  Future<void> loadImages() async{
-   images =await _picker.pickMultiImage();
 
-   //화면갱신
-   setState(() {
+  Future<void> loadImages() async {
+    images = await _picker.pickMultiImage();
 
-   });
+    //화면갱신
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('전자액자')),
+        title: Text('전자액자'),
+      ),
+      body: images == null
+          ? Center(child: Text('No data'))
+          : FutureBuilder<Uint8List>(
+              future: images![0].readAsBytes(),
+              builder: (context, snapshot) {
+                final data = snapshot.data;
+
+                if (data == null ||
+                    snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                return Image.memory(
+                    data,
+                    width: double.infinity,
+                );
+              }),
     );
   }
 }
